e11e56bd473c5d7d6d06cf1104e13e4b12ccd40c
JLFranca007 <jlfranca07@gmail.com>
2025-11-27 11:55:43 -0300

    Troca-de-mensagens-MQTT

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const MQTT_URL = "wss://test.mosquitto.org:8081";
 const clienteId = "web-casa-inteligente-jose";
 
 // ### Criando o cliente MQRR do navegador, POREM ainda não estamos conectados ao BROKER
-// const cliente = mqtt.connect(MQTT_URL, {
-//     clienteId,
-//     clean: true,
-//     connectTimeout: 4000,
-// });
+const cliente = mqtt.connect(MQTT_URL, {
+    clienteId,
+    clean: true,
+    connectTimeout: 4000,
+});
 
 // ####### Variaveis dos dispositivos
 const lampSalaInput = document.getElementById("lamp-sala");
@@ -43,146 +43,185 @@ const IrVarTexto = document.getElementById("ir-varanda-texto");
 // ####### Alterando estado dos dispositivos
 
 // pegando o evento de mudança do botão (ligado/desligado)
-lampSalaInput.addEventListener("change", () => {
-    // Verifica se o esta como ligado
-    const ligado = lampSalaInput.checked === true;
-
-    if(ligado === true){
-        console.log("Lampada ligada");
-        lampSalaTexto.innerHTML = "Ligado";
-    }   else{
-        console.log("Lampada desligada");
-        lampSalaTexto.innerHTML = "Desligado";
-    }
-});
-
-cortSalaInput.addEventListener("change", () =>{
-    const ligado = cortSalaInput.checked === true;
-
-    if(ligado === true){
-        console.log("Cortina aberta");
-        cortSalaTexto.innerHTML = "Aberta";
-    } else {
-        console.log("Cortina fechada");
-        cortSalaTexto.innerHTML = "Fechada";
-    }
-});
-
-portSalaInput.addEventListener("change", () => {
-    const ligado = portSalaInput.checked === true;
-
-    if(ligado === true){
-        console.log("Porta Aberta");
-        portSalaTexto.innerHTML = "Aberta";
-    } else {
-        console.log("Porta Fechada");
-        portSalaTexto.innerHTML = "Fechada";
-    }
-});
-
-lampCoziInput.addEventListener("change", () => {
-    const ligado = lampCoziInput.checked === true;
-
-    if(ligado === true){
-        console.log("Lampada ligada");
-        lampCoziTexto.innerHTML = "Ligada";
-    } else {
-        console.log("Lampada desligada");
-        lampCoziTexto.innerHTML = "Desligada";
-    }
-});
-
-exauCoziInput.addEventListener("change", () => {
-    const ligado = exauCoziInput.checked === true;
-
-    if(ligado === true){
-        console.log("Exaustor ligado");
-        exauCoziTexto.innerHTML = "Ligado";
-    } else {
-        console.log("Exautor desligado");
-        exauCoziTexto.innerHTML = "Desligado";
-    }
-});
 
 
-lampVarInput.addEventListener("change", () => {
-    const ligado = lampVarInput.checked === true;
 
-    if(ligado === true){
-        console.log("Lampada ligada");
-        lampVarTexto.innerHTML = "Ligado";
-    } else {
-        console.log("Lampada desligada");
-        lampVarTexto.innerHTML = "Desligado";
-    }
-});
-
-VarVarInput.addEventListener("change", () => {
-    const ligado = VarVarInput.checked === true;
-
-    if(ligado === true){
-        console.log("Varal aberto");
-        VarVarTexto.innerHTML = "Ligado";
-    } else {
-        console.log("Varal fechado");
-        VarVarTexto.innerHTML = "Desligado";
-    }
-});
-
-PortVarInput.addEventListener("change", () => {
-    const ligado = PortVarInput.checked === true;
-
-    if(ligado === true){
-        console.log("Porta aberta");
-        PortVarTexto.innerHTML = "Ligado";
-    } else {
-        console.log("Porta desligado");
-        PortVarTexto.innerHTML = "Desligado";
-    }
-});
-
-IrVarInput.addEventListener("change", () => {
-    const ligado = IrVarInput.checked === true;
-
-    if(ligado === true){
-        console.log("Irrigador ligado");
-        IrVarTexto.innerHTML = "Ligado";
-    } else {
-        console.log("Irrigador desligado");
-        IrVarTexto.innerHTML = "Desligado";
-    }
-});
 
 
 
 
 // ### chamando um evento do JS do tipo "DOMContentLoaded", que é
 // o evento que acontece após toda a minha página de HTML ser carregada
-// document.addEventListener("DOMContentLoaded", () => {
-//     console.log("Página carregada com sucesso ✅;.. Conectando ao Mosquitto!");
-//     // ### Estabelecendo a conexão com o broker do mqtt
-//     cliente.on("connect", () => {
-//         console.log("Conexão estabelecida com Sucesso ✅!");
-//         console.log("Cliente conectado: ", clienteId);
-        
-//         // ### criando um topico para acessar mensagens do MQTT
-//         const topicoTeste = "teste/jose";
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Página carregada com sucesso ✅;.. Conectando ao Mosquitto!");
+    // ### Estabelecendo a conexão com o broker do mqtt
+    cliente.on("connect", () => {
+        console.log("Conexão estabelecida com Sucesso ✅!");
+        console.log("Cliente conectado: ", clienteId);
 
-//         // ### recebendo mensagens do topico criado
-//         cliente.subscribe(topicoTeste);
-//     });
+        // ### criando um topico para acessar mensagens do MQTT
+        const topicoTeste = "teste/jose";
 
-//     // ### Preparando mensagem de erro caso algo aconteça
-//     cliente.on("error", () => {
-//         console.error("Erro ao conecttar ao Broker MQTT 🚫!");
-//         console.error("erro");
-//     });
+        // ### recebendo mensagens do topico criado
+        cliente.subscribe(topicoTeste);
+    });
 
-//     // ### Recebendo as mensagens dos tópicos assinados no MQTT pelo cliente
-//     cliente.on("message", (topico, mensagem) => {
-//         console.log("Topico rebido: ", topico);
-//         console.log("Mensagem recebida: ", mensagem);
-//     });
-// });
+    // ### Preparando mensagem de erro caso algo aconteça
+    cliente.on("error", () => {
+        console.error("Erro ao conecttar ao Broker MQTT 🚫!");
+        console.error("erro");
+    });
+
+    // ### Recebendo as mensagens dos tópicos assinados no MQTT pelo cliente
+    cliente.on("message", (topico, mensagem) => {
+        console.log("Topico rebido: ", topico);
+        console.log("Mensagem recebida: ", mensagem.toString());
+    });
+
+    lampSalaInput.addEventListener("change", () => {
+        const TOPICO_LAMP_SALA = 'casa-jose/sala/lamp';
+
+        // Verifica se o dispositivo esta como ligado
+        const ligado = lampSalaInput.checked === true;
+
+        if (ligado === true) {
+            // console.log("Lampada ligada");
+            cliente.publish(TOPICO_LAMP_SALA, 'Ligado');
+            lampSalaTexto.innerHTML = "Ligado";
+        } else {
+            // console.log("Lampada desligada");
+            cliente.publish(TOPICO_LAMP_SALA, 'Desligado');
+            lampSalaTexto.innerHTML = "Desligado";
+        }
+    });
+
+    cortSalaInput.addEventListener("change", () => {
+        const TOPICO_CORT_SALA = 'casa-jose/sala/cort';
+
+        const ligado = cortSalaInput.checked === true;
+
+        if (ligado === true) {
+            // console.log("Cortina aberta");
+            cliente.publish(TOPICO_CORT_SALA, 'Aberta');
+            cortSalaTexto.innerHTML = "Aberta";
+        } else {
+            // console.log("Cortina fechada");
+            cliente.publish(TOPICO_CORT_SALA, 'Fechada');
+            cortSalaTexto.innerHTML = "Fechada";
+        }
+    });
+
+    portSalaInput.addEventListener("change", () => {
+        const TOPICO_PORT_SALA = 'casa-jose/sala/port';
+         
+        const ligado = portSalaInput.checked === true;
+
+        if (ligado === true) {
+            // console.log("Porta Aberta");
+            cliente.publish(TOPICO_PORT_SALA, 'Aberta');
+            portSalaTexto.innerHTML = "Aberta";
+        } else {
+            // console.log("Porta Fechada");
+            cliente.publish(TOPICO_PORT_SALA, 'Fechada');
+            portSalaTexto.innerHTML = "Fechada";
+        }
+    });
+
+    lampCoziInput.addEventListener("change", () => {
+        const TOPICO_LAMP_COZI = 'casa-jose/cozi/lamp';
+
+        const ligado = lampCoziInput.checked === true;
+
+        if (ligado === true) {
+            // console.log("Lampada ligada");
+            cliente.publish(TOPICO_LAMP_COZI, 'Ligada');
+            lampCoziTexto.innerHTML = "Ligada";
+        } else {
+            // console.log("Lampada desligada");
+            cliente.publish(TOPICO_LAMP_COZI, 'Desligada');
+            lampCoziTexto.innerHTML = "Desligada";
+        }
+    });
+
+    exauCoziInput.addEventListener("change", () => {
+        const TOPICO_EXAU_COZI = 'casa-jose/cozi/exau';
+
+        const ligado = exauCoziInput.checked === true;
+
+        if (ligado === true) {
+            // console.log("Exaustor ligado");
+            cliente.publish(TOPICO_EXAU_COZI, 'Ligado');
+            exauCoziTexto.innerHTML = "Ligado";
+        } else {
+            // console.log("Exautor desligado");
+            cliente.publish(TOPICO_EXAU_COZI, 'Desligado');
+            exauCoziTexto.innerHTML = "Desligado";
+        }
+    });
 
 
+    lampVarInput.addEventListener("change", () => {
+        const TOPICO_LAMP_VAR = 'casa-jose/var/lamp';
+        const ligado = lampVarInput.checked === true;
+
+        if (ligado === true) {
+            // console.log("Lampada ligada");
+            cliente.publish(TOPICO_LAMP_VAR, 'Ligado');
+            lampVarTexto.innerHTML = "Ligado";
+        } else {
+            // console.log("Lampada desligada");
+            cliente.publish(TOPICO_LAMP_VAR, 'Desligado');
+            lampVarTexto.innerHTML = "Desligado";
+        }
+    });
+
+    VarVarInput.addEventListener("change", () => {
+        const TOPICO_VAR_VAR = 'casa-jose/var/var';
+
+        const ligado = VarVarInput.checked === true;
+
+        if (ligado === true) {
+            // console.log("Varal aberto");
+            cliente.publish(TOPICO_VAR_VAR, 'Ligado');
+            VarVarTexto.innerHTML = "Ligado";
+        } else {
+            // console.log("Varal fechado");
+            cliente.publish(TOPICO_VAR_VAR, 'Desligado');
+            VarVarTexto.innerHTML = "Desligado";
+        }
+    });
+
+    PortVarInput.addEventListener("change", () => {
+        const TOPICO_PORT_VAR = 'casa-jose/var/port';
+
+        const ligado = PortVarInput.checked === true;
+
+        if (ligado === true) {
+            // console.log("Porta aberta");
+            cliente.publish(TOPICO_PORT_VAR, 'Ligado');
+            PortVarTexto.innerHTML = "Ligado";
+        } else {
+            // console.log("Porta desligado");
+            cliente.publish(TOPICO_PORT_VAR, 'Desligado');
+            PortVarTexto.innerHTML = "Desligado";
+        }
+    });
+
+    IrVarInput.addEventListener("change", () => {
+        const TOPICO_IR_VAR = 'casa-jose/var/ir';
+
+        const ligado = IrVarInput.checked === true;
+
+        if (ligado === true) {
+            // console.log("Irrigador ligado");
+            cliente.publish(TOPICO_IR_VAR, 'Ligado');
+            IrVarTexto.innerHTML = "Ligado";
+        } else {
+            // console.log("Irrigador desligado");
+            cliente.publish(TOPICO_IR_VAR, 'Desligado');
+            IrVarTexto.innerHTML = "Desligado";
+        }
+    });
+
+
+});
